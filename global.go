f5f113cf86b7d04a19a00bbc6138a348c662c154
gitlab123 <flwwd@outlook.com>
2019-06-16 17:26:00 +0800

    optimize

--- global.go
+++ global.go
@@ -4,6 +4,11 @@ import "github.com/microcosm-cc/bluemonday"
 
 func init()  {
 	bluemondayPolicy=bluemonday.NewPolicy()
+	bluemondayPolicy.AllowStandardURLs()
+	bluemondayPolicy.AllowAttrs("href").OnElements("a", "area")
+	bluemondayPolicy.AllowAttrs("src").OnElements("img")
+	bluemondayPolicy.AllowElements("hl","h2","h3","h4","h5","h6")
+	bluemondayPolicy.AllowElements("p","ol","li","br")
 }
 
 var bluemondayPolicy *bluemonday.Policy
