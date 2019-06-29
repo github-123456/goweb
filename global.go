4feee2ebf8cc1b21d26c556bf70a18d4b3d5499f
gitlab123 <flwwd@outlook.com>
2019-06-30 00:11:12 +0800

    sanitize

--- global.go
+++ global.go
@@ -7,8 +7,9 @@ func init()  {
 	bluemondayPolicy.AllowStandardURLs()
 	bluemondayPolicy.AllowAttrs("href").OnElements("a", "area")
 	bluemondayPolicy.AllowAttrs("src").OnElements("img")
+	bluemondayPolicy.AllowAttrs("class").OnElements("code","span")
 	bluemondayPolicy.AllowElements("h1","h2","h3","h4","h5","h6")
-	bluemondayPolicy.AllowElements("p","ol","li","br")
+	bluemondayPolicy.AllowElements("p","ol","li","br","pre","code","span")
 }
 
 var bluemondayPolicy *bluemonday.Policy
