88963021a29b22c02ac1214ca89c6b51acaf4bfc
gitlab123 <flwwd@outlook.com>
2019-07-02 20:03:05 +0800

    check if server is updating

--- page.go
+++ page.go
@@ -12,7 +12,7 @@ type PageModel interface {
 func RenderPage(c *Context,pageModel PageModel, filenames ...string) {
 	tmpl,err:= template.ParseFiles(filenames...)
 	if err != nil {
-		fmt.Fprintf(c.Writer, err.Error())
+		fmt.Fprintf(c.Writer,"server update is in progress...please wait a moment")
 		return
 	}
 	pageModel.Prepare(c)
